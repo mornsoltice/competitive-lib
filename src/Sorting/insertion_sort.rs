ecba6522aa30f9f222cdef83975cf7eea8a91a33
mornsoltice <kafianjingkucingbebek@gmail.com>
2024-09-02 07:10:03 +0700

    i love venti

--- src/Sorting/insertion_sort.rs
+++ src/Sorting/insertion_sort.rs
@@ -3,10 +3,11 @@ pub fn insertion_sort(arr: &mut [i32]) {
     for i in 1..n {
         let key = arr[i];
         let mut j = i as isize - 1;
+
         while j >= 0 && arr[j as usize] > key {
-            arr[j as usize + 1] = arr[j as usize];
+            arr[(j + 1) as usize] = arr[j as usize];
             j -= 1;
         }
-        arr[j as usize + 1] = key;
+        arr[(j + 1) as usize] = key;
     }
 }
